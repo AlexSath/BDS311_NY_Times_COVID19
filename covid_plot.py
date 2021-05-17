67ad112da7420abffb34d12c6fec98a489ff369c
Kei Horikawa <horikkei@oregonstate.edu>
2021-05-17 01:26:26 +0000

    created get_extreme_states function, but not sure

--- covid_plot.py
+++ covid_plot.py
@@ -11,7 +11,7 @@ def read_in_data(filename):
     return df
 
 def get_extreme_states(df, num_states):
-     '''
+    '''
     INPUT: 
     df: Pandas dataframe with raw state data
     num_states: The number of states to include on either extreme end (e.g. 5 will return 5 states with lowest and overall average covid
@@ -20,7 +20,17 @@ def get_extreme_states(df, num_states):
     low_states: states with lowest covid cases
     high_states: states with highest covid cases
     '''
-    ...
+    low_states=[]
+    high_states=[]
+    states_df=df[['state','cases_avg_per_100k']].groupby('state').agg('mean')
+    states_df.reset_index(inplace=True)
+    sorted_df=states_df.sort_values('cases_avg_per_100k').head(num_states)
+    reverse_sorted_df=states_df.sort_values('cases_avg_per_100k',ascending=False).head(num_states)
+    low_states.append(sorted_df['state'])
+    high_states.append(reverse_sorted_df['state'])
+    return low_states, high_states
+    
+
           
 def make_plot(df,states_to_plot):
      '''
@@ -30,7 +40,7 @@ def make_plot(df,states_to_plot):
     OUTPUT: 
     ax: handle to current plot
     '''
-    ...
+    
         
     
    
@@ -43,6 +53,6 @@ def modify_plot(ax,states_to_plot):
  
     '''
     
-    ...
+
   
    
